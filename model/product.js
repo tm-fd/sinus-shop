5011dc4dd05137f2cc0034a567d55f2f0005111c
Traiq <mr.tm84@gmail.com>
2021-05-19 08:59:07 +0200

    Update product.js

--- model/product.js
+++ model/product.js
@@ -8,6 +8,6 @@ const productSchema = mongoose.Schema({
     imgFile:String,
 });
 
-const Product = mongoose.model('Product', productsSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
