e9abe76042c6253d5e174a6f71a4308ad3d54ec1
Traiq <mr.tm84@gmail.com>
2021-05-19 09:53:44 +0200

    new mongoose

--- model/order.js
+++ model/order.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const orderSchema = mongoose.Schema({
+const orderSchema = new mongoose.Schema({
     timeStamp: Date.now(),
     status: Boolean,
     items: [  
---
+++ routes/orders.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+async function(productId, order){
+const product = await Product.findById(productId)
+product.orders.push(order)
+product.save()
+}
