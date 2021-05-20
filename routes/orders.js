c10f2c4f604118cf48ba839d714e0beb72960a82
Medine <you@example.com>
2021-05-20 10:49:39 +0200

    under correction

--- routes/orders.js
+++ routes/orders.js
@@ -4,62 +4,37 @@ const Order = require('../model/order');
 const Product = require('../model/product');
 const mongoose = require('mongoose');
 
-const jwt = require('jsonwebtoken')
+
+const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 router.use(cookieParser());
-//const bcrypt = require('bcrypt');
+
 require('dotenv').config()
-/* const options = {
-    algorithm: 'HS256'
-} */
+
 
 router.get('/api/orders', async (req, res) => {
     console.log(req.cookies['auth-token']);
     if (!req.cookies['auth-token']) {
         res.status(204).send("No Content - Bara för inloggade")
     } else {
-
-        const token = req.cookies['auth-token'];
-        jwt.verify(token, process.env.SECRET, async (err, payload) => {
+        const user = await User.findOne({ name: req.body.name })
+      
+            bcrypt.compare(req.body.password, user.password, async (err, payload) => {
             if (err) {
                 res.json(err)
             } else {
-                //const user = await User.findOne({ name: req.body.name, password: req.body.password});
-
-                /* if (user.role === 'admin') {
-                    const orders = await Order.find();
-                    console.log(JSON.stringify(orders));
-                    res.json(orders);
-                } else if (user.role === 'kund') {
-                    const user = await User.findOne({ name: req.body.name, password: req.body.password },
-                        { orderHistory: 1 }).populate('orderHistory');
-                    res.json(user.orderHistory);
-                } */
-
-                if (payload.role === 'admin') {
+    
+                if (user.role === 'admin') {
                     const orders = await Order.find();
                     console.log(JSON.stringify(orders));
                     res.json(orders);
                 } else {
                     const user = await User.findOne({ name: req.body.name, password: req.body.password },
                         { orderHistory: 1 }).populate('orderHistory');
-                    res.json(user.orderHistory);
+                   res.json(user.orderHistory);
+                
                 }
-            
-
             } 
-
-
-
-
-
-
-
-
-
-
-
-
         })
     }
 });
@@ -70,26 +45,29 @@ router.post('/api/orders', async (req, res) => {
     if (!req.cookies['auth-token']) {
         res.send("Bara för inloggade.")
     } else {
-        const token = req.cookies['auth-token']
-        jwt.verify(token, process.env.SECRET, /* options,  */async(err, payload) => {
+        const user = await User.findOne({ name: req.body.name })
+     
+        bcrypt.compare(req.body.password, user.password, async (err, payload) => {
             if (err) {
                 res.json(err)
             } else {
               
-                const user = await User.findOne({ name: req.body.name/* , password: req.body.password */ });
+                const user = await User.findOne({ name: req.body.name , password: req.body.password  });
 
     let items = req.body.items.split(',');
     console.log(req.body.items);
     items = items.map(el => mongoose.Types.ObjectId(el));
+    
+    
     // Validate products
-
+ 
     const products = await Product.find({ _id: { $in: items } });
-
+/* 
     if (items.length !== products.length) {
-        // Do something
-        res.status(400);
+        // meddelande
+        res.status(400).send("Det är tomt här !!!!!");
         return;
-    }
+    }  */
 
     let order = new Order({
         timeStamp: Date.now(),
@@ -101,8 +79,11 @@ router.post('/api/orders', async (req, res) => {
 
     await Order.create(order);
 
+
+
+
     //hittar rätt user och updaterar orders info
-    const result = await User.findByIdAndUpdate(user._id, { $push: { orderHistory: order._id }});
+    const result = await User.findByIdAndUpdate(user._id, { $push: { orderHistory: order._id }});//w
     //const result = await User.findById(user._id).update({ $push: { orderHistory: order._id } });
     if (result.updatedCount == 0) {
         // Didn't work
