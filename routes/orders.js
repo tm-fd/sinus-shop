d87fdbc5c051bdb418a5c0448a0448e11e6e4c12
Traiq <mr.tm84@gmail.com>
2021-05-20 09:19:30 +0200

    auth user - AuthController

---
+++ routes/orders.js
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+=======
+
+
+
+
+
+/*
+async function(productId, order){
+const product = await Product.findById(productId)
+product.orders.push(order)
+product.save()
+}
+
+*/
+>>>>>>> Stashed changes
