36403922b325de673df281ecd5ae41ff0287163a
tim.varvikko <tim.varvikko59@gmail.com>
2021-05-21 10:01:43 +0200

    test update

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
 const products = require('./routes/products');
 const auth = require('./controller/authController');
 const users = require('./routes/register');
 const order = require('./routes/orders');
-const mongoose = require('mongoose');
-const app = express()
 
-app.use( express.static('public') )
 
 
 // connect to mongodb & listen for requests
 const dbURI = "mongodb+srv://Grin:projektgrin@grin.0ubep.mongodb.net/Grin_db?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, dbName: 'Tims-Test-Database' })
   .catch(err => console.log(err));
 
 
 // Middleware
+app.use( express.static('public') );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
@@ -28,4 +29,4 @@ app.use('/api/register', users);
 app.use('/', order);
 
 
-module.exports = app
+module.exports = app;
