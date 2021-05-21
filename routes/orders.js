112122d47864e274c641785581bc61b8ed048cdb
Medine <you@example.com>
2021-05-21 19:08:21 +0200

    order- funkar

--- routes/orders.js
+++ routes/orders.js
@@ -2,36 +2,36 @@ const router = require('express').Router();
 const User = require('../models/user');
 const Order = require('../models/order');
 const Product = require('../models/product');
-//const mongoose = require('mongoose');
-//const jwt = require('jsonwebtoken')
 
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 router.use(cookieParser());
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config()
 
 router.get('/api/orders', async (req, res) => {
-    // checking if not cookies from tokens then 204 status.
-    if (!req.cookie['auth-token']) {
-        res.status(204).send("No Content - Bara för inloggade")
-        // else the rest is inside, which means only for logged in users.
+    console.log(req.cookies['auth-token']);
+    if (!req.cookies['auth-token']) {
+        res.send("Bara för inloggade.")
     } else {
-        const user = await User.findOne({ name: req.body.name })
-
-        bcrypt.compare(req.body.password, user.password, async (err, result) => {
+        const token = req.cookies['auth-token'];
+        jwt.verify(token, process.env.SECRET, async (err, payload) => {
+            const user = await User.findOne({ name: payload.user.name })
             if (err) {
                 res.json(err)
             } else {
                 if (user.role === 'admin') {
                     const orders = await Order.find();
                     res.json(orders);
-                } else {
-                    const user = await User.findOne({ name: req.body.name, password: req.body.password }, { orderHistory: 1 }).populate('orderHistory');
+                } 
+                else if(user.role === 'kund') {
+                    const user = await User.findOne({ name: payload.user.name }, { orderHistory: 1 }).populate('orderHistory');
                     res.json(user.orderHistory);
                 }
             }
         })
+
     }
 });
 
@@ -41,18 +41,24 @@ router.post('/api/orders', async (req, res) => {
     if (!req.cookies['auth-token']) {
         res.send("Bara för inloggade.")
     } else {
-        const user = await User.findOne({ name: req.body.name })
-        bcrypt.compare(req.body.password, user.password, async result => {
-            if (!result) {
-                // Failed authentication 
+        const token = req.cookies['auth-token'];
+
+
+        jwt.verify(token, process.env.SECRET, async (err, payload) => {
+            if (err) {
+                res.json(err)
+
+
             } else {
-                const user = await User.findOne({ name: req.body.name });
+                const user = await User.findOne({ name: payload.user.name })
                 let items = req.body.items;
-                if(!items){
-                    res.status(404).send('finns inget');
-                    return;
-                }
+             
+
+                (items === undefined || items === null || items.length === 0 ) ?  res.status(404).send('FEL') : res.status(200).send('BRA')
+
+
                 const allProducts = await Product.find({ _id: { $in: items } });
+
                 let order = new Order({
                     timeStamp: Date.now(),
                     status: true,
@@ -61,7 +67,8 @@ router.post('/api/orders', async (req, res) => {
                 });
 
                 await Order.create(order);
-
+                console.log('user._id', user._id)
+                console.log('order._id', order._id)
                 const result = await User.findByIdAndUpdate(user._id, { $push: { orderHistory: order._id } });
                 console.log(result)
 
