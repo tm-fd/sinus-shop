0bc53cd0247044c15c224cfe7fbfad9e4564ea91
Traiq <mr.tm84@gmail.com>
2021-05-23 01:21:37 +0200

    total order value solved -

--- routes/orders.js
+++ routes/orders.js
@@ -26,7 +26,7 @@ router.get('/api/orders', async (req, res) => {
                     const orders = await Order.find();
                     res.json(orders);
                 } 
-                else if(user.role === 'kund') {
+                else if(user.role === 'customer') {
                     const user = await User.findOne({ name: payload.user.name }, { orderHistory: 1 }).populate('orderHistory');
                     res.json(user.orderHistory);
                 }
@@ -53,25 +53,49 @@ router.post('/api/orders', async (req, res) => {
             } else {
                 const user = await User.findOne({ name: payload.user.name })
                 let items = req.body.items;
-             
+                
                 // kollar ifall det inte finns några varor i varukorgen, då skapas det inga ordrar.
                 (items === undefined || items === null || items.length === 0 ) ?  res.status(404).send('FEL') : res.status(200).send('BRA')
-
+                
                 // den kollar i produkt modelen och checkar ifall rätt id till produkt matchar inne i items i order model.
-                const allProducts = await Product.find({ _id: { $in: items } });
+               // let allProducts = await Product.find({ _id: { $in: items }});
+
+                ////////////////////////////////////////////////////////////////////
+                // get price for each product in the order, then return sum
+                async function orderSum(productsArray) {
+                    let totalSum = 0;
+                    // find matching products by comparing all products and pruducts in the order 
+                    try {
+                        let products = await Product.find();
+                        for (let i = 0; i < products.length; i++) {
+                            for (let x = 0; x < productsArray.length; x++) {
+                                if (products[i]._id == productsArray[x]) {
+                                    totalSum += products[i].price
+                                }
+                            }
+                        }
+                        return totalSum
+                    } catch (error) {
+                        return error
+                    }
+                }
+
+                const sumResult = await orderSum(req.body.items)
+                console.log(sumResult)
+                //////////////////////////////////////////////////////////////
 
                 let order = new Order({
                     timeStamp: Date.now(),
                     status: true,
                     items: items,
-                    orderValue: allProducts.reduce((total, prod) => total + prod.price, 0)
+                    orderValue: sumResult
                 });
                 // Skapar order och lägger till den i new Order.
                 await Order.create(order);
                 // den hittar rätt user med id och uppdaterar personens orderHistory.
                 const result = await User.findByIdAndUpdate(user._id, { $push: { orderHistory: order._id } });
 
-                res.json(order);
+               // res.json(order);
             }
         })
     }
