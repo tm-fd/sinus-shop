a79949f2e2657c8078b2f0b09b993884135a1993
elinkronqvist <elin.kronqvist@utb.ecutbildning.se>
2021-05-24 00:05:42 +0200

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -59,64 +59,9 @@ router.post('/', async (req, res) => {
         })
     }  else {
         //Annars kör denna: emailadressen finns redan registrerad
-        return res.status(400).send(`${req.body.email} already registered`)
+        return res.status(400).send(`${req.body.email} is already registered`)
     }
 })
 
 
 module.exports = router;
-
-
-
-/* ---------- */
-
-// //Registrera ny användare
-// router.post('/', (req, res) => {
-//     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-//         if (err)
-//         res.json(err)
-//         else {
-//             const newUser = new User({
-//                 // _id: new mongoose.Types.ObjectId(),
-//                 name: req.body.name,
-//                 email: req.body.email,
-//                 password: hash,
-//                 role: 'customer',
-//                 adress: {
-//                     street: req.body.street,
-//                     zip: req.body.zip,
-//                     city: req.body.city,
-//                 },
-//                 orderHistory: []
-//             })
-
-//             // const email = User.findOne({ email: req.body.email })
-
-//             if (req.body.email == true) {
-//                 newUser.save((err) =>{
-//                     if(err) {
-//                         console.error(err)
-//                     }
-//                     else {
-//                         // res.json(newUser) 
-//                     	return res.status(400).send(`Ny användare registrerad!`)
-//                     }
-//                 })
-
-//             } else {
-//                 return res.status(400).send("E-mail redan registrerad")
-//             }
-
-//             // const error = newUser.validateSync()
-//             // if (error) {
-//             //     res.send(error.message)
-//             // } else {
-//             //     res.json(newUser)
-//             // }
-
-//         }
-//     })
-// })
-
-
-// module.exports = router;
