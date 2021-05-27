bdb267e9da039dd5004146749f7e54863d1cd756
Traiq <mr.tm84@gmail.com>
2021-05-27 13:09:21 +0200

    comment authorization.js

--- controller/authorization.js
+++ controller/authorization.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const authorizationMiddleware = async (req, res, next) => {
-
+// read the token in the cookies 
     const token = req.cookies['auth-token'];
+// if it was empty stop the proecess by return response message
     if(!token) return res.status(401).send('Access denied. You are not authorize')
 
     try{
+        // decode the token in the cookies, get the paylood then read and verfiy the secret with the in the server
         const decodedToken = jwt.verify(token, process.env.SECRET);
+        // access the decoded token by assign it to the request
         req.decodedToken = decodedToken;
+        // move to the next middleware
         next();
     }
     catch(exp){
